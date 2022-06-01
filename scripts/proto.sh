527f0cf88f5b4ca86b990f18106ada6c5b9113d0
awsdesktop\chrisma <council_tax@hotmail.com>
2022-06-01 18:05:08 +0100

    generate csharp proto

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -12,6 +12,33 @@ protoc \
 --gogofaster_out=$TYPES,plugins=grpc:./ \
 pkg/api/*.proto
 
+# protoc dotnet client
+protoc \
+--proto_path=. \
+--proto_path=/proto \
+--plugin=protoc-gen-grpc=/usr/local/bin/grpc_csharp_plugin \
+--grpc_out=client/DotNet/Armada.Client/generated \
+--csharp_out=client/DotNet/Armada.Client/generated \
+    google/api/annotations.proto \
+    google/api/http.proto \
+    pkg/api/event.proto pkg/api/queue.proto pkg/api/submit.proto \
+    github.com/gogo/protobuf/gogoproto/gogo.proto
+
+# proto dotnet k8s.  We need to do this in a separate step bso we can add a --csharp_opt=base_namespace=K8S.Io flag
+# which forces files to be created in different directories and thus gets round multiple "generated.cs" files being created
+protoc \
+--proto_path=/proto \
+--csharp_out=client/DotNet/Armada.Client/generated \
+--csharp_opt=base_namespace=K8S.Io \
+    k8s.io/api/core/v1/generated.proto \
+    k8s.io/apimachinery/pkg/api/resource/generated.proto \
+    k8s.io/apimachinery/pkg/apis/meta/v1/generated.proto \
+    k8s.io/apimachinery/pkg/runtime/generated.proto \
+    k8s.io/apimachinery/pkg/runtime/schema/generated.proto \
+    k8s.io/apimachinery/pkg/util/intstr/generated.proto \
+    k8s.io/api/networking/v1/generated.proto
+
+
 protoc \
 --proto_path=. \
 --proto_path=/proto \
@@ -65,3 +92,4 @@ protoc \
 --grpc-gateway_out=logtostderr=true,$TYPES:. \
 --swagger_out=logtostderr=true,$TYPES,allow_merge=true,simple_operation_ids=true,json_names_for_fields=true,merge_file_name=./pkg/api/binoculars/api:. \
 pkg/api/binoculars/binoculars.proto \
+
